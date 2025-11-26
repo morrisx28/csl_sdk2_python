c434e79bb35a40375a1d2331e1c801005ab13153
morris <morrisx28@gmail.com>
2025-11-26 09:14:12 +0800

    fix er

--- csl_sdk2py/utils/crc.py
+++ csl_sdk2py/utils/crc.py
@@ -14,13 +14,10 @@ class CRC(Singleton):
     def __init__(self):
         #4 bytes aligned, little-endian format.
         #size 812
-        self.__packFmtLowCmd = '<4B4IH2x' + 'B3x5f3I' * 20 + '4B' + '55Bx2I'
+        # self.__packFmtLowCmd = '<4B4IH2x' + 'B3x5f3I' * 20 + '4B' + '55Bx2I' # '4B' = bms state
+        self.__packFmtLowCmd = '<4B4IH2x' + 'B3x5f3I' * 20 + '55Bx2I'
         #size 1180
         self.__packFmtLowState = '<4B4IH2x' + '13fb3x' + 'B3x7fb3x3I' * 20 + '4BiH4b15H' + '8hI41B3xf2b2x2f4h2I'
-        #size 1004
-        self.__packFmtHGLowCmd = '<2B2x' + 'B3x5fI' * 35 + '5I'
-        #size 2092
-        self.__packFmtHGLowState = '<2I2B2xI' + '13fh2x' + 'B3x4f2hf7I' * 35 + '40B5I'
 
         
         script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -35,14 +32,10 @@ class CRC(Singleton):
             self.crc_lib.crc32_core.restype = ctypes.c_uint32
     
     def Crc(self, msg: idl.IdlStruct):
-        if msg.__idl_typename__ == 'unitree_go.msg.dds_.LowCmd_':
+        if msg.__idl_typename__ == 'csl_pineapple.msg.dds_.LowCmd_':
             return self.__Crc32(self.__PackLowCmd(msg))
-        elif msg.__idl_typename__ == 'unitree_go.msg.dds_.LowState_':
+        elif msg.__idl_typename__ == 'csl_pineapple.msg.dds_.LowState_':
             return self.__Crc32(self.__PackLowState(msg))
-        if msg.__idl_typename__ == 'unitree_hg.msg.dds_.LowCmd_':
-            return self.__Crc32(self.__PackHGLowCmd(msg))
-        elif msg.__idl_typename__ == 'unitree_hg.msg.dds_.LowState_':
-            return self.__Crc32(self.__PackHGLowState(msg))
         else:
             raise TypeError('unknown IDL message type to crc')
 
@@ -63,9 +56,6 @@ class CRC(Singleton):
             origData.append(cmd.motor_cmd[i].kp)
             origData.append(cmd.motor_cmd[i].kd)
             origData.extend(cmd.motor_cmd[i].reserve)
-
-        origData.append(cmd.bms_cmd.off)
-        origData.extend(cmd.bms_cmd.reserve)
 
         origData.extend(cmd.wireless_remote)
         origData.extend(cmd.led)
@@ -104,15 +94,6 @@ class CRC(Singleton):
             origData.append(state.motor_state[i].lost)
             origData.extend(state.motor_state[i].reserve)
 
-        origData.append(state.bms_state.version_high)
-        origData.append(state.bms_state.version_low)
-        origData.append(state.bms_state.status)
-        origData.append(state.bms_state.soc)
-        origData.append(state.bms_state.current)
-        origData.append(state.bms_state.cycle)
-        origData.extend(state.bms_state.bq_ntc)
-        origData.extend(state.bms_state.mcu_ntc)
-        origData.extend(state.bms_state.cell_vol)
         
         origData.extend(state.foot_force)
         origData.extend(state.foot_force_est)
@@ -129,56 +110,6 @@ class CRC(Singleton):
         origData.append(state.crc)
 
         return self.__Trans(struct.pack(self.__packFmtLowState, *origData))
-
-    def __PackHGLowCmd(self, cmd: HGLowCmd_):
-        origData = []
-        origData.append(cmd.mode_pr)
-        origData.append(cmd.mode_machine)
-
-        for i in range(35):
-            origData.append(cmd.motor_cmd[i].mode)
-            origData.append(cmd.motor_cmd[i].q)
-            origData.append(cmd.motor_cmd[i].dq)
-            origData.append(cmd.motor_cmd[i].tau)
-            origData.append(cmd.motor_cmd[i].kp)
-            origData.append(cmd.motor_cmd[i].kd)
-            origData.append(cmd.motor_cmd[i].reserve)
-
-        origData.extend(cmd.reserve)
-        origData.append(cmd.crc)
-
-        return self.__Trans(struct.pack(self.__packFmtHGLowCmd, *origData))
-
-    def __PackHGLowState(self, state: HGLowState_):
-        origData = []
-        origData.extend(state.version)
-        origData.append(state.mode_pr)
-        origData.append(state.mode_machine)
-        origData.append(state.tick)
-        
-        origData.extend(state.imu_state.quaternion)
-        origData.extend(state.imu_state.gyroscope)
-        origData.extend(state.imu_state.accelerometer)
-        origData.extend(state.imu_state.rpy)
-        origData.append(state.imu_state.temperature)
-        
-        for i in range(35):
-            origData.append(state.motor_state[i].mode)
-            origData.append(state.motor_state[i].q)
-            origData.append(state.motor_state[i].dq)
-            origData.append(state.motor_state[i].ddq)
-            origData.append(state.motor_state[i].tau_est)
-            origData.extend(state.motor_state[i].temperature)
-            origData.append(state.motor_state[i].vol)
-            origData.extend(state.motor_state[i].sensor)
-            origData.append(state.motor_state[i].motorstate)
-            origData.extend(state.motor_state[i].reserve)
-
-        origData.extend(state.wireless_remote)
-        origData.extend(state.reserve)
-        origData.append(state.crc)
-
-        return self.__Trans(struct.pack(self.__packFmtHGLowState, *origData))
 
     def __Trans(self, packData):
         calcData = []
